c7889e1c6931f970f7c8c17ae82216df0a6c3827
Tony Chen <36219739+chentoast@users.noreply.github.com>
2022-05-18 15:23:26 -0400

    fix: error in setup on nil config (#68)

--- lua/marks/init.lua
+++ lua/marks/init.lua
@@ -189,6 +189,8 @@ local function setup_autocommands()
 end
 
 function M.setup(config)
+  config = config or {}
+
   M.mark_state = mark.new()
   M.mark_state.builtin_marks = config.builtin_marks or {}
 
