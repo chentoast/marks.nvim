7ed61925e07b0028e4b31ba58878430673b612a3
Jed <vjeudy@protonmail.com>
2024-02-26 23:54:50 +0100

    renames telescope extension to avoid conflicts

--- lua/telescope/_extensions/marks_nvim.lua
+++ lua/telescope/_extensions/marks_nvim.lua
@@ -1,9 +1,4 @@
-local has_telescope, telescope = pcall(require, "telescope")
-
-if not has_telescope then
-  error("using marks.nvim telescope extensions requires nvim-telescope/telescope.nvim")
-end
-
+local telescope = require("telescope")
 local pickers = require("telescope.pickers")
 local finders = require("telescope.finders")
 local entry_display = require("telescope.pickers.entry_display")
@@ -26,8 +21,47 @@ local list_buf = function(opts)
   local make_display = function(entry)
     return displayer({
       { entry.mark, "TelescopeResultsIdentifier" },
-      { entry.lnum },
+      { "l " .. entry.lnum },
       { entry.line, "String" },
+    })
+  end
+
+  pickers
+    .new(opts or {}, {
+      prompt_title = "Buffer Marks",
+      finder = finders.new_table({
+        results = results,
+        entry_maker = function(entry)
+          entry.value = entry.mark
+          entry.ordinal = entry.line
+          entry.display = make_display
+          return entry
+        end,
+      }),
+      sorter = conf.generic_sorter(opts),
+      previewer = conf.grep_previewer(opts),
+    })
+    :find()
+end
+
+local list_all = function(opts)
+  opts = opts or {}
+  local results = marks.mark_state:get_all_list() or {}
+
+  local displayer = entry_display.create({
+    separator = " ",
+    items = {
+      { width = 3 },
+      { width = 10 },
+      {},
+    },
+  })
+
+  local make_display = function(entry)
+    return displayer({
+      { entry.mark, "TelescopeResultsIdentifier" },
+      { "l " .. entry.lnum },
+      { entry.filename, "String" },
     })
   end
 
@@ -51,8 +85,7 @@ end
 
 return telescope.register_extension({
   exports = {
-    marks_list_buf = function(opts)
-      list_buf(opts)
-    end,
+    marks_list_buf = list_buf,
+    marks_list_all = list_all
   },
 })
