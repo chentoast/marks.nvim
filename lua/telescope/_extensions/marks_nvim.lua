0fe9ed85ad0caf8b11219995bd26723fd46af319
Jed <vjeudy@protonmail.com>
2024-03-03 16:54:20 +0100

    adds telescope support for bookmarks too

--- lua/telescope/_extensions/marks_nvim.lua
+++ lua/telescope/_extensions/marks_nvim.lua
@@ -5,87 +5,173 @@ local entry_display = require("telescope.pickers.entry_display")
 local conf = require("telescope.config").values
 local marks = require("marks")
 
-local list_buf = function(opts)
-  opts = opts or {}
-  local results = marks.mark_state:get_buf_list() or {}
+local list_marks_buf = function(opts)
+	opts = opts or {}
+	local results = marks.mark_state:get_buf_list() or {}
 
-  local displayer = entry_display.create({
-    separator = " ",
-    items = {
-      { width = 3 },
-      { width = 10 },
-      {},
-    },
-  })
+	local displayer = entry_display.create({
+		separator = " ",
+		items = {
+			{ width = 3 },
+			{ width = 10 },
+			{},
+		},
+	})
 
-  local make_display = function(entry)
-    return displayer({
-      { entry.mark, "TelescopeResultsIdentifier" },
-      { "l " .. entry.lnum },
-      { entry.line, "String" },
-    })
-  end
+	local make_display = function(entry)
+		return displayer({
+			{ entry.mark, "TelescopeResultsIdentifier" },
+			{ "l" .. entry.lnum },
+			{ entry.line, "String" },
+		})
+	end
 
-  pickers
-    .new(opts or {}, {
-      prompt_title = "Buffer Marks",
-      finder = finders.new_table({
-        results = results,
-        entry_maker = function(entry)
-          entry.value = entry.mark
-          entry.ordinal = entry.line
-          entry.display = make_display
-          return entry
-        end,
-      }),
-      sorter = conf.generic_sorter(opts),
-      previewer = conf.grep_previewer(opts),
-    })
-    :find()
+	pickers
+		.new(opts or {}, {
+			prompt_title = "Buffer Marks",
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(entry)
+					entry.value = entry.mark
+					entry.ordinal = entry.line
+					entry.display = make_display
+					return entry
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			previewer = conf.grep_previewer(opts),
+		})
+		:find()
 end
 
-local list_all = function(opts)
-  opts = opts or {}
-  local results = marks.mark_state:get_all_list() or {}
+local list_marks_all = function(opts)
+	opts = opts or {}
+	local results = marks.mark_state:get_all_list() or {}
 
-  local displayer = entry_display.create({
-    separator = " ",
-    items = {
-      { width = 3 },
-      { width = 10 },
-      {},
-    },
-  })
+	local displayer = entry_display.create({
+		separator = " ",
+		items = {
+			{ width = 1 },
+			{ width = 7 },
+			{ width = 15 },
+			{},
+		},
+	})
 
-  local make_display = function(entry)
-    return displayer({
-      { entry.mark, "TelescopeResultsIdentifier" },
-      { "l " .. entry.lnum },
-      { entry.filename, "String" },
-    })
-  end
+	local make_display = function(entry)
+		return displayer({
+			{ entry.mark, "TelescopeResultsIdentifier" },
+			{ "l" .. entry.lnum },
+			{ entry.line, "String" },
+			{ vim.fs.normalize(entry.filename) },
+		})
+	end
 
-  pickers
-    .new(opts or {}, {
-      prompt_title = "All Marks",
-      finder = finders.new_table({
-        results = results,
-        entry_maker = function(entry)
-          entry.value = entry.mark
-          entry.ordinal = entry.line
-          entry.display = make_display
-          return entry
-        end,
-      }),
-      sorter = conf.generic_sorter(opts),
-      previewer = conf.grep_previewer(opts),
-    })
-    :find()
+	pickers
+		.new(opts or {}, {
+			prompt_title = "All Marks",
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(entry)
+					entry.value = entry.mark
+					entry.ordinal = entry.line
+					entry.display = make_display
+					return entry
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			previewer = conf.grep_previewer(opts),
+		})
+		:find()
+end
+
+local list_bookmarks_group = function(group, opts)
+	opts = opts or {}
+	local results = marks.bookmark_state:get_group_list(group) or {}
+
+	local displayer = entry_display.create({
+		separator = " ",
+		items = {
+			{ width = 1 },
+			{ width = 7 },
+			{ width = 15 },
+			{},
+		},
+	})
+
+	local make_display = function(entry)
+		return displayer({
+			{ entry.group, "TelescopeResultsIdentifier" },
+			{ "l" .. entry.lnum },
+			{ entry.line, "String" },
+			{ vim.fs.normalize(entry.filename) },
+		})
+	end
+
+	pickers
+		.new(opts or {}, {
+			prompt_title = "Bookmark " .. group,
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(entry)
+					entry.value = entry.group
+					entry.ordinal = entry.line
+					entry.display = make_display
+					return entry
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			previewer = conf.grep_previewer(opts),
+		})
+		:find()
+end
+
+local list_bookmarks_all = function(opts)
+	opts = opts or {}
+	local results = marks.bookmark_state:get_all_list() or {}
+
+	local displayer = entry_display.create({
+		separator = " ",
+		items = {
+			{ width = 1 },
+			{ width = 7 },
+			{ width = 15 },
+			{},
+		},
+	})
+
+	local make_display = function(entry)
+		return displayer({
+			{ entry.group, "TelescopeResultsIdentifier" },
+			{ "l" .. entry.lnum },
+			{ entry.line, "String" },
+			{ vim.fs.normalize(entry.filename) },
+		})
+	end
+
+	pickers
+		.new(opts or {}, {
+			prompt_title = "All Bookmarks",
+			finder = finders.new_table({
+				results = results,
+				entry_maker = function(entry)
+					entry.value = entry.group
+					entry.ordinal = entry.line
+					entry.display = make_display
+					return entry
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+			previewer = conf.grep_previewer(opts),
+		})
+		:find()
 end
 
 return telescope.register_extension({
-  exports = {
-    marks_list_buf = list_buf,
-    marks_list_all = list_all
-  },
+	exports = {
+		marks_list_buf = list_marks_buf,
+		marks_list_all = list_marks_all,
+		bookmarks_list_group = list_bookmarks_group,
+		bookmarks_list_all = list_bookmarks_all,
+	},
 })
